30179ed743fd7585f65e0525a7a17f6d13f91181
DIFFFFFFT <672228275@qq.com>
2024-08-20 17:27:11 +0800

    fix style

---
+++ app/src/main/assets/scripts/index.js
@@ -0,0 +1,3 @@
+// 隐藏打开APP的弹窗
+//const popup = document.querySelector("shreddit-async-loader[bundlename='app_selector']");
+//if (popup) popup.style.display = 'none';
--- app/src/main/java/com/leyen/reddit/MainActivity.java
+++ app/src/main/java/com/leyen/reddit/MainActivity.java
@@ -2,12 +2,14 @@ package com.leyen.reddit;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
@@ -28,36 +30,81 @@ import java.io.InputStreamReader;
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
+    private WebChromeClient webChromeClient;
     private boolean doubleBackToExitPressedOnce = false;
 
-    @SuppressLint("SetJavaScriptEnabled")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+    public void pushCss(String cssPath) {
+        try {
+            InputStream inputStream = getAssets().open(cssPath);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder cssBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                cssBuilder.append(line).append("\n");
+            }
+            reader.close();
+            String css = cssBuilder.toString();
+            // 注入CSS到网页
+            String js = "var style = document.createElement('style');" +
+                    "style.type = 'text/css';" +
+                    "style.innerHTML = `" + css + "`;" +
+                    "document.head.appendChild(style);";
+            webView.evaluateJavascript(js, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void pushJs(String jsPath) {
+        try {
+            InputStream inputStream = getAssets().open(jsPath);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder jsBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                jsBuilder.append(line).append("\n");
+            }
+            reader.close();
+            String js = jsBuilder.toString();
+            webView.evaluateJavascript(js, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setSystemBars() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        //
         Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.status_bar_color));
-        webView = findViewById(R.id.webView);
+    }
 
-        WebChromeClient webChromeClient = new WebChromeClient();
+    public void setWebChromeClient() {
+        webChromeClient = new WebChromeClient();
         webView.setWebChromeClient(webChromeClient);
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    public void setWebSettings() {
         webView.getSettings().setDomStorageEnabled(true);
         webView.getSettings().setDatabaseEnabled(true);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setAllowFileAccessFromFileURLs(true);
         webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
         webView.getSettings().setSupportZoom(true);
-//        webView.loadUrl("file:///android_asset/index.html");
-        // 禁止android打开外部应用
+        webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+    }
+
+    public void loadAssets(String htmlPath) {
+        webView.loadUrl("file:///android_asset/" + htmlPath + ".html");
+    }
+
+    public void setWebViewClient() {
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -65,45 +112,36 @@ public class MainActivity extends AppCompatActivity {
                     // 让 WebView 加载这个 URL
                     return false;
                 } else {
-                    // 在外部浏览器中打开这个 URL
-//                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-//                    startActivity(intent);
+                    // Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    // startActivity(intent);
                     return true;
                 }
             }
 
             @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageCommitVisible(WebView view, String url) {
+                super.onPageCommitVisible(view, url);
+                pushCss("styles/index.css");
+            }
+
+            @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
-
-                // 读取CSS文件内容
-                try {
-                    InputStream inputStream = getAssets().open("style/style.css");
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                    StringBuilder cssBuilder = new StringBuilder();
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        cssBuilder.append(line).append("\n");
-                    }
-                    reader.close();
-                    String css = cssBuilder.toString();
-                    // 注入CSS到网页
-                    String js = "var style = document.createElement('style');" +
-                            "style.type = 'text/css';" +
-                            "style.innerHTML = `" + css + "`;" +
-                            "document.head.appendChild(style);";
-                    webView.evaluateJavascript(js, null);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                pushJs("scripts/index.js");
             }
         });
-        webView.setWebContentsDebuggingEnabled(true);
-        webView.loadUrl("https://www.reddit.com/");
+    }
 
+    public void setDebug() {
+        WebView.setWebContentsDebuggingEnabled(true);
+    }
 
-//        https://www.reddit.com/?rdt=42985
-
+    public void setBackPressedCallback() {
         OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
             @Override
             public void handleOnBackPressed() {
@@ -124,5 +162,24 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         getOnBackPressedDispatcher().addCallback(this, callback);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        setSystemBars();
+
+        webView = findViewById(R.id.webView);
+        setDebug();
+        setWebChromeClient();
+        setWebViewClient();
+        setWebSettings();
+        webView.loadUrl("https://www.reddit.com/");
+
+        setBackPressedCallback();
     }
 }
